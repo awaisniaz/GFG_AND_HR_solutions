481e63e4193f31eddde301af6133c185171a1dc3
awais.niaz@mwanmobile.org <awais.niaz@mwanmobile.org>
2023-08-04 22:58:12 +0500

    Distict Element from Array

---
+++ DisticElement.py
@@ -0,0 +1,8 @@
+def DisticElemen(arr):
+    newarr = []
+    for i in range(len(arr)):
+        if(i == arr.index(arr[i])):
+            newarr.append(arr[i])
+    print(newarr)
+
+DisticElemen([1, 2, 3, 4, 5])
