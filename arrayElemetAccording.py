c120afe3952ee796a7c980751abb4328cc83ad5c
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2023-09-24 20:34:17 +0500

    Update arrayElemetAccording.py

--- arrayElemetAccording.py
+++ arrayElemetAccording.py
@@ -4,6 +4,5 @@ def ArrangeWithIndex(indexarr, arr):
     for i in range(len(arr)):
         newarr[indexarr[i]] = arr[i]
     print(newarr)
-
 if __name__ == "__main__":
     ArrangeWithIndex([1, 0, 2], [10, 11, 12])
